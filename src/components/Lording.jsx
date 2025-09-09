e2c5568029908e8dea3dd1143aa319cbc626f89c
Shamil Kaleel <155573196+ShamilKaleel@users.noreply.github.com>
2025-09-09 18:49:40 +0530

    Fix class attribute to className for JSX compliance

--- src/components/Lording.jsx
+++ src/components/Lording.jsx
@@ -2,10 +2,10 @@ export default function Lording() {
   return (
     <div className="mx-auto  md:max-w-screen-xl mt-[30%]">
       <div className="mx-5 2xl:mx-0">
-        <div class="flex flex-row gap-2">
-          <div class="w-4 h-4 rounded-full bg-lime-600 animate-bounce [animation-delay:.7s]"></div>
-          <div class="w-4 h-4 rounded-full bg-lime-600 animate-bounce [animation-delay:.3s]"></div>
-          <div class="w-4 h-4 rounded-full bg-lime-600 animate-bounce [animation-delay:.7s]"></div>
+        <div className="flex flex-row gap-2">
+          <div className="w-4 h-4 rounded-full bg-lime-600 animate-bounce [animation-delay:.7s]"></div>
+          <div className="w-4 h-4 rounded-full bg-lime-600 animate-bounce [animation-delay:.3s]"></div>
+          <div className="w-4 h-4 rounded-full bg-lime-600 animate-bounce [animation-delay:.7s]"></div>
         </div>
       </div>
     </div>
